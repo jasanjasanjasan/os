18d08f979991704efecd70c12c40358f4b0858bf
@pantoflicekj <pantoflicekj@ssemi.cz>
2024-01-21 13:58:34 +0100

    added a string print function tried to read from the key board

--- boot.asm
+++ boot.asm
@@ -1,27 +1,32 @@
+[org  0x7c00]
 mov ah, 0x0e
-mov al, 65
-int 0x10
+mov bx, buffer
 
+char:
+ db 0
+ mov ah, 0
+ int 0x16
+ mov al, [char]
 
-loop:
- inc al
- cmp al, 90 + 1
- je exit
+buffer:
+ times 10 db 0
+ mov bx, buffer
+ mov [bx], al
+ inc bx 
+
+print:
+ mov al, [bx]
+ cmp al, 0
+ je end
  int 0x10
- jmp loop_big
+ inc bx
+ jmp print
 
-loop_big:
- inc al
- mov bl, al
- add al, 32
- int 0x10
- mov al, bl
- cmp al, 90 + 1
- je exit
- jmp loop
+end:
+ jmp $
 
-exit:
-    jmp $
+text:
+ db "the man didnt know he was all ready dead", 0
+
 times 510 - ($-$$) db 0
-
 db 0x55,0xaa
